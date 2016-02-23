df6d3fd792324513bf57e00a028640e4585779f3
Sam Lane <demoncoder38@gmail.com>
2016-02-22 21:22:43 -0600

    adding stringutil package

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/samlane/go-fun/stringutil"
+)
 
 func main() {
     fmt.Printf("Hello, World\n")
+    fmt.Printf(stringutil.Reverse("Hello, World\n"))
 }
